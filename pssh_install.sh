d4e5432909cacc72e4e93b5cb193e8265fc6d9fb
shaowei2019 <15090825090@163.com>
2019-03-13 01:37:18 -0400

    delete pssh

--- pssh_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-filename="pssh-2.3.1"
-if [ -f ${filename}.tar.gz ];then
-	tar -zxf ${filename}.tar.gz && cd ${filename} && python setup.py install
-else
-	echo "${filename} IS NOT FOUND!"
-fi
